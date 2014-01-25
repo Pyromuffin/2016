19a5941b3b9fcc5829056ef18665a143705f4298
Benji Smith <smith.benj@husky.neu.edu>
2014-01-25 01:17:02 -0500

    Actual commit, last one was a fluke.  Basic pathfinding.

---
+++ Assets/Scripts/PathToGoal.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathToGoal : MonoBehaviour {
+
+	public Transform goalPoint;
+	private NavMeshAgent navigation;
+
+	// Use this for initialization
+	void Start () {
+		//goalPoint = GameObject.FindGameObjectWithTag("Player").transform;
+		navigation = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(goalPoint != null){
+			navigation.SetDestination(goalPoint.position);
+		}
+	}
+}
