51673f1be8f7da6132db5e8af99c5efe1a71830f
Kelly MacNeill <kelly@pyromuffin>
2014-01-25 02:19:21 -0500

    enemy sound effects

---
+++ Assets/GhostBeeping.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostBeeping : MonoBehaviour {
+    public float beepRate = .5f;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("beep", 0, .5f);
+	}
+
+
+    public void beep()
+    {
+        audio.Play();
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    
+
+	}
+}
