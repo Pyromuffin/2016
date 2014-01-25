7895ae26bf9355da55a164b7b450f8a39cfeac00
Kelly MacNeill <kelly@pyromuffin>
2014-01-25 01:21:13 -0500

    basic player controls and prototype assets

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform followTarget;
+    public float viewAngle = 60;
+    public float followDistance = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = followTarget.position;
+        transform.forward = followTarget.forward;
+        transform.Rotate(new Vector3(viewAngle, 0, 0));
+        transform.position -= transform.forward * followDistance;
+	}
+}
