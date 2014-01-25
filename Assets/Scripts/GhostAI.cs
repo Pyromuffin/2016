ac1e5084d0f26f3b3c2b21e0d7f70b38524287a5
Benji Smith <smith.benj@husky.neu.edu>
2014-01-25 11:30:27 -0500

    Commit?

---
+++ Assets/Scripts/GhostAI.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostAI : MonoBehaviour {
+
+	private PathToGoal pathManager;
+    private StepSounds stepSounds;
+    private GameObject player;
+
+	public GameObject[] patrolPoints;
+	private GameObject currentPatrolPoint;
+
+	private bool seesPlayer = false;
+	private bool isChasingPlayer = false;
+   
+
+	public LayerMask seePlayerLayer;
+	public float seePlayerDistance = 50.0f;
+	public float seePlayerFOV = 0.6f; //Cosine of the FOV angle the ghost can see the player in
+	public float chasePlayerTime = 8.0f;
+	private float timeSinceLastSawPlayer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		patrolPoints = GameObject.FindGameObjectsWithTag("Patrol");
+		pathManager = GetComponent<PathToGoal>();
+		player = GameObject.FindGameObjectWithTag("Player");
+		currentPatrolPoint = RandomPatrolPoint();
+        stepSounds = GetComponent<StepSounds>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(CanSeePlayer()){
+			seesPlayer = true;
+			isChasingPlayer = true;
+			timeSinceLastSawPlayer = 0.0f;
+		}
+		else{
+			seesPlayer = false;
+			timeSinceLastSawPlayer += Time.deltaTime;
+		}
+
+		if(isChasingPlayer){
+			pathManager.goalPoint = player.transform;
+            audio.pitch = 1.5f;
+            stepSounds.stepDistance = 1;
+			if(!seesPlayer && timeSinceLastSawPlayer >= chasePlayerTime){
+				isChasingPlayer = false;
+                audio.pitch = 1f;
+                stepSounds.stepDistance = 3;
+			}
+		}
+		else{
+			pathManager.goalPoint = currentPatrolPoint.transform;
+		}	
+	}
+
+	void OnTriggerEnter(Collider col){
+		if(col.tag == "Patrol" && col.gameObject == currentPatrolPoint){
+			currentPatrolPoint = RandomPatrolPoint();
+		}
+	}
+
+	GameObject RandomPatrolPoint(){
+		return patrolPoints[Random.Range(0,patrolPoints.Length)];
+	}
+
+	bool CanSeePlayer(){
+		RaycastHit hit;
+		//If Raycast its something
+		if(Physics.Raycast(transform.position, (player.transform.position - transform.position), out hit, seePlayerDistance, seePlayerLayer)){
+			//If it hits player
+			if(hit.transform.tag == "Player"){ 
+				//If the player is within the ghost's FOV
+				if(Vector3.Dot((player.transform.position - transform.position).normalized, transform.forward) > seePlayerFOV){
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+}
---
+++ Assets/Scripts/StepSounds.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(AudioLowPassFilter))]
+[RequireComponent(typeof(AudioReverbFilter))]
+public class StepSounds : MonoBehaviour {
+    public float stepDistance = .2f;
+    Vector3 previousStepPosition;
+    public AudioClip[] stepSounds;
+    GameObject player;
+    AudioLowPassFilter lowPass;
+    AudioReverbFilter reverb;
+
+	// Use this for initialization
+	void Start () {
+        previousStepPosition = transform.position;
+        player = GameObject.FindGameObjectWithTag("Player");
+        lowPass = GetComponent<AudioLowPassFilter>();
+        reverb = GetComponent<AudioReverbFilter>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if ((previousStepPosition - transform.position).magnitude >= stepDistance)
+        {
+           
+
+            audio.clip = stepSounds[Random.Range(0, stepSounds.Length)];
+            
+            if (Physics.Linecast(player.transform.position, transform.position, 1 << LayerMask.NameToLayer("Walls") ) )
+            {
+                //there is something between 
+                lowPass.enabled = true;
+                audio.volume = .25f;
+                reverb.enabled = true;
+            }
+            else
+            {
+                lowPass.enabled = false;
+                reverb.enabled = false;
+                audio.volume = 1;
+            }
+            audio.Play();
+            previousStepPosition = transform.position;
+
+        }
+
+
+	}
+}
