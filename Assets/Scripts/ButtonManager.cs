3a17a32057cd53a1cb076fe38dac846bfe99d358
Benji Smith <smith.benj@husky.neu.edu>
2014-01-26 12:47:34 -0500

    Proper UI buttons working, and health/ghost bars no longer have missing scripts. ACTUAL COMMIT.

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour {
+
+	public int levelId;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void QuitGame(){
+		Application.Quit();
+	}
+
+	void StartGame(){
+		Application.LoadLevel(levelId);
+	}
+}
