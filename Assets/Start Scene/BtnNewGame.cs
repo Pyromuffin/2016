664d31c6e7c241afeaf670f30ae676c3d627b9fe
Seth Alter <subalterngames@gmail.com>
2014-01-25 20:36:37 -0500

    I am committing!
    I have trouble making commitments.

---
+++ Assets/Start Scene/BtnNewGame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BtnNewGame : MonoBehaviour {
+
+	tk2dUIItem ui;
+	
+	// Use this for initialization
+	void Start () {
+		ui = GetComponent<tk2dUIItem> ();
+		ui.OnClick += NewGame;
+	}
+
+	void NewGame(){
+		Application.LoadLevel ("OK SAVE THE SCENE");
+	}
+}
---
+++ Assets/Start Scene/BtnQuit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BtnQuit : MonoBehaviour {
+	tk2dUIItem ui;
+
+	// Use this for initialization
+	void Start () {
+		ui = GetComponent<tk2dUIItem> ();
+		ui.OnClick += QuitGame;
+	}
+
+	void QuitGame(){
+		Application.Quit ();
+	}
+}
