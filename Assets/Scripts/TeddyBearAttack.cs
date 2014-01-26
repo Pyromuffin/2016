5b37f99a2fb581374dfd08d6e7b061d485ccca43
Kelly MacNeill <kelly@pyromuffin>
2014-01-26 07:27:12 -0500

    dubstep, I'm so sorry.

--- Assets/Scripts/TeddyBearAttack.cs
+++ Assets/Scripts/TeddyBearAttack.cs
@@ -7,8 +7,17 @@ public class TeddyBearAttack : MonoBehaviour {
 	public float teddyRange = 5.0f;
 	public string teddyName = "TeddyBear";
     TeddyCharging teddyCharging;
+    public ParticleSystem bearParticles;
+    public GameObject wub;
+    public float wubRate;
+    AudioLowPassFilter wubFilter;
+    AudioSource wubDispenser;
+    public float cutoffIncrease = 100;
+    public AudioClip wob;
 
 	public float chargeTime = 1.0f;
+    float wubTimer = 0;
+    Transform cameraController;
 
 	private float timeButtonHeldDown = 0.0f;
 
@@ -16,7 +25,11 @@ public class TeddyBearAttack : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        cameraController = FindObjectOfType<OVRCameraController>().transform;
         teddyCharging = FindObjectOfType<TeddyCharging>();
+        var wubGO = GameObject.Find("Wub Dispenser");
+        wubDispenser = wubGO.GetComponent<AudioSource>();
+        wubFilter = wubGO.GetComponent<AudioLowPassFilter>();
 	}
 	
 	// Update is called once per frame
@@ -26,12 +39,25 @@ public class TeddyBearAttack : MonoBehaviour {
 		if(timeButtonHeldDown >= chargeTime && Input.GetButtonUp(buttonName)){
 			AttackWithBear();
 		}
+        if (Input.GetButtonDown(buttonName))
+            wubDispenser.Play();
 
 		if(Input.GetButton(buttonName)){
+            
 			timeButtonHeldDown += Time.deltaTime;
+            wubTimer += Time.deltaTime;
+            wubFilter.cutoffFrequency += Time.deltaTime * cutoffIncrease;
+            if (wubTimer > wubRate) { 
+               Instantiate(wub,bearParticles.transform.position + cameraController.forward, Quaternion.identity);
+               wubTimer = 0;
+            }
+
 		}
 		else{
+            wubDispenser.Stop();
+            audio.PlayOneShot(wob, 1);
 			timeButtonHeldDown = 0.0f;
+            wubFilter.cutoffFrequency = 100;
 		}
 		
 	}
---
+++ Assets/WubGrow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WubGrow : MonoBehaviour {
+
+    public float wubTime = 3f;
+    public float wubSpeed = 2;
+    Transform player;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, wubTime);
+        player = FindObjectOfType<OVRCameraController>().transform;
+        transform.localScale = Vector3.one * Random.value * 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale += Vector3.one *  wubSpeed * Time.deltaTime;
+        transform.forward = player.forward;
+	}
+}
