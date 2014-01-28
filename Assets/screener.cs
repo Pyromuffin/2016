eed8528fa6ad7eeb9e7a27a545f0a1a58535cacf
Benji Smith <smith.benj@husky.neu.edu>
2014-01-27 20:24:50 -0500

    Can toggle whether or not using the rift.

--- Assets/screener.cs
+++ Assets/screener.cs
@@ -5,12 +5,12 @@ public class screener : MonoBehaviour {
 
 	// Use this for initialization
 	void Awake () {
-        Screen.SetResolution(1920 * 2, 1080, false);
+       //Screen.SetResolution(1920 * 2, 1080, false);
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//Debug.Log(Screen.currentResolution.width);
 	}
 }
